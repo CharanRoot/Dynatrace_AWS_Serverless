a6abe731de7758d62f795441caba06bbe9cb73c8
CharanRoot <charan.sjd@gmail.com>
2019-10-16 11:24:12 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: "node",
+  mode: "production",
+  output: {
+    libraryTarget: "commonjs",
+    path: path.join(__dirname, ".webpack"),
+    filename: "[name].js"
+  }
+};
